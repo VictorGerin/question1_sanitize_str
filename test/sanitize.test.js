be2612c3346fc3d5dae3bb7100497007977dd651
Victor Lacerda <gerinlacerda@gmail.com>
2021-09-28 00:51:40 -0300

    Add new cases validation

--- test/sanitize.test.js
+++ test/sanitize.test.js
@@ -1,7 +1,7 @@
 import sanitize from "../src/sanitize";
 
 //font http://shakespeare.mit.edu/hamlet/full.html
-//some qoute was add for testing reasons
+//some quote was add for testing reasons
 var text =
   "Good Hamlet, cast thy nighted colour off,\n" +
   "And let thine eye look like a friend on Denmark.\n" +
@@ -10,12 +10,28 @@ var text =
   "Thou know'st 'tis common; \"all\" that lives must die,\n" +
   "Passing through nature to eternity.\n";
 
+var textSmartQuote =
+  "Good Hamlet, cast thy nighted colour off,\n" +
+  "And let thine eye look like a friend on Denmark.\n" +
+  "Do not for ever with thy vailed “lids\n" +
+  "Seek for thy noble father in the” dust:\n" +
+  "Thou know'st 'tis common; “all” that lives must die,\n" +
+  "Passing through nature to eternity.\n";
+
 var sanitizedText =
   "Good Hamlet, cast thy nighted colour off,\n" +
   "And let thine eye look like a friend on Denmark.\n" +
   "Do not for ever with thy vailed lids\n" +
   "Seek for thy noble father in the dust:\n" +
   "Thou know'st 'tis common; all that lives must die,\n" +
+  "Passing through nature to eternity.\n";
+
+var singleQuoteSanitized =
+  "Good Hamlet, cast thy nighted colour off,\n" +
+  "And let thine eye look like a friend on Denmark.\n" +
+  'Do not for ever with thy vailed "lids\n' +
+  'Seek for thy noble father in the" dust:\n' +
+  'Thou knowst tis common; "all" that lives must die,\n' +
   "Passing through nature to eternity.\n";
 
 var sanitizedTextEscaped =
@@ -91,3 +107,38 @@ test("Order operation sanitize", () => {
 
   expect(sanitize(text, conf)).toBe(sanitizedTextEscaped);
 });
+
+test("Smart quote sanitize", () => {
+  var conf = {
+    smartQuote: true,
+  };
+
+  expect(sanitize("Hello “Victor Lacerda” World", conf)).toBe(
+    "Hello Victor Lacerda World"
+  );
+  expect(sanitize("Hello “Victor\nLacerda” World", conf)).toBe(
+    "Hello Victor\nLacerda World"
+  );
+  expect(sanitize(textSmartQuote, conf)).toBe(sanitizedText);
+});
+
+test("Single quote sanitize", () => {
+  var conf = {
+    quote: "'",
+  };
+
+  expect(sanitize("Hello 'Victor Lacerda' World", conf)).toBe(
+    "Hello Victor Lacerda World"
+  );
+  expect(sanitize("Hello 'Victor\nLacerda' World", conf)).toBe(
+    "Hello Victor\nLacerda World"
+  );
+
+  expect(sanitize(text, conf)).toBe(singleQuoteSanitized);
+});
+
+test("No args test", () => {
+  expect(() => {
+    sanitize()
+  }).toThrow(TypeError);
+})
